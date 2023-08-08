c82975731e74ba49a5fe75769016ae99e2a5036a
Maria <globalkmaria@gmail.com>
2023-08-08 23:42:13 +0900

    add : stock api

--- router/v1/index.js
+++ router/v1/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import authRouter from "./auth.js";
+import stocksRouter from "./stocks.js";
 
 const router = express.Router();
 
 router.use("/auth", authRouter);
+router.use("/stocks", stocksRouter);
 
 export default router;
---
+++ router/v1/stocks.js
@@ -0,0 +1,61 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const router = express.Router();
+
+const stockSchema = new mongoose.Schema({
+  rank: Number,
+  name: String,
+  symbol: String,
+  description: String,
+  industry: String,
+  sector: String,
+  financial: {
+    market_cap: String,
+    revenue: String,
+    gross_profit: String,
+    operating_income: String,
+  },
+  ratios: {
+    per: Number,
+    pbr: Number,
+    roa: Number,
+  },
+  img_url: String,
+  invest_url: String,
+});
+
+const Stock = new mongoose.model("Stock", stockSchema);
+
+router.get("/", async (req, res) => {
+  const stocks = await mongoose.model("Stock").aggregate([
+    {
+      $project: {
+        id: "$_id",
+        rank: 1,
+        name: 1,
+        symbol: 1,
+        description: 1,
+        industry: 1,
+        sector: 1,
+        financial: {
+          marketCap: "$financial.market_cap",
+          revenue: 1,
+          grossProfit: "$financial.gross_profit",
+          operatingIncome: "$financial.operating_income",
+        },
+        ratios: {
+          per: 1,
+          pbr: 1,
+          roa: 1,
+        },
+        imgUrl: "$img_url",
+        investUrl: "$invest_url",
+      },
+    },
+  ]);
+  res.setHeader("Cache-Control", "public, max-age=3600");
+  res.send(stocks);
+});
+
+export default router;
