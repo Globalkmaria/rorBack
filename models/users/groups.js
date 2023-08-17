533964d833d4beabfc612c2ad5c19ea1f4b9c1ce
Maria <globalkmaria@gmail.com>
2023-08-18 02:23:56 +0900

    Add : timestamps to stocks and groups

--- models/users/groups.js
+++ models/users/groups.js
@@ -3,37 +3,43 @@ import { userConnection } from "../../db/database.js";
 
 const Schema = mongoose.Schema;
 
-const GroupSchema = new Schema({
-  id: {
-    type: String,
-    required: true,
+const GroupSchema = new Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    stocks: {
+      type: Map,
+      of: [String],
+      required: true,
+    },
   },
-  name: {
-    type: String,
-    required: true,
-  },
-  stocks: {
-    type: Map,
-    of: [String],
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
-const userGroupsSchema = new Schema({
-  user_id: {
-    type: mongoose.Types.ObjectId,
-    required: true,
+const userGroupsSchema = new Schema(
+  {
+    user_id: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+    },
+    next_group_id: {
+      type: String,
+      required: true,
+    },
+    groups: {
+      type: Map,
+      of: GroupSchema,
+      required: true,
+    },
   },
-  next_group_id: {
-    type: String,
-    required: true,
-  },
-  groups: {
-    type: Map,
-    of: GroupSchema,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const userGroups = userConnection.model("Group", userGroupsSchema);
 export default userGroups;
--- models/users/stocks.js
+++ models/users/stocks.js
@@ -3,44 +3,25 @@ import { userConnection } from "../../db/database.js";
 
 const Schema = mongoose.Schema;
 
-const ItemSchema = new Schema({
-  id: {
-    type: String,
-    required: true,
-  },
-  buy_date: {
-    type: Date,
-    required: true,
-  },
-  buy_time: {
-    type: String,
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    required: true,
-  },
-  buy_price: {
-    type: Number,
-    required: true,
-  },
-  created_at: {
-    type: Date,
-    required: true,
-  },
-});
-
-const StockSchema = new Schema({
-  info: {
+const ItemSchema = new Schema(
+  {
     id: {
       type: String,
       required: true,
     },
-    name: {
+    buy_date: {
+      type: Date,
+      required: true,
+    },
+    buy_time: {
       type: String,
       required: true,
     },
-    current_price: {
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    buy_price: {
       type: Number,
       required: true,
     },
@@ -49,33 +30,61 @@ const StockSchema = new Schema({
       required: true,
     },
   },
-  items: {
-    type: Map,
-    of: ItemSchema,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
-export const userStocksSchema = new Schema({
-  _id: mongoose.Types.ObjectId,
-  user_id: {
-    type: mongoose.Types.ObjectId,
-    required: true,
+const StockSchema = new Schema(
+  {
+    info: {
+      id: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      current_price: {
+        type: Number,
+        required: true,
+      },
+      created_at: {
+        type: Date,
+        required: true,
+      },
+    },
+    items: {
+      type: Map,
+      of: ItemSchema,
+      required: true,
+    },
   },
-  next_stock_id: {
-    type: Number,
-    required: true,
+  { timestamps: true }
+);
+
+export const userStocksSchema = new Schema(
+  {
+    _id: mongoose.Types.ObjectId,
+    user_id: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+    },
+    next_stock_id: {
+      type: Number,
+      required: true,
+    },
+    next_item_id: {
+      type: Number,
+      required: true,
+    },
+    stocks: {
+      type: Map,
+      of: StockSchema,
+      required: true,
+    },
   },
-  next_item_id: {
-    type: Number,
-    required: true,
-  },
-  stocks: {
-    type: Map,
-    of: StockSchema,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const userStocks = userConnection.model("Stock", userStocksSchema);
 
